1aa588ebeca275dbbd524a17f7caaebfbb707e72
Fushihara <1039534+fushihara@users.noreply.github.com>
2020-12-31 19:08:27 +0900

    行間を修正。hoge_kage のアンダーバーが表示されなかったので。

--- src/esaCodeCopy.ts
+++ src/esaCodeCopy.ts
@@ -2,14 +2,14 @@ const styleRuleInsertLog: WeakMap<Document, boolean> = new WeakMap();
 const cssPrefix = `k777`;
 export class EsaCodeCopy {
   constructor(private readonly element: HTMLDivElement) {
-    const fileName = element.querySelector<HTMLElement>(".code-filename").innerText;
-    const insertElement = document.createElement("div");
+    const fileName = element.querySelector<HTMLElement>('.code-filename').innerText;
+    const insertElement = document.createElement('div');
     insertElement.classList.add(cssPrefix);
-    insertElement.classList.add("code-block__copy-button");
+    insertElement.classList.add('code-block__copy-button');
     for (let v of getButtons(fileName)) {
       insertElement.appendChild(v.button);
     }
-    element.querySelector(".highlight").appendChild(insertElement);
+    element.querySelector('.highlight').appendChild(insertElement);
     EsaCodeCopy.insertStyleRule();
   }
   private static insertStyleRule() {
@@ -22,36 +22,36 @@ export class EsaCodeCopy {
     styleEl.sheet.insertRule(`.markdown .code-block__copy-button.${cssPrefix}{ position: absolute;top: -25px;right: 0;border:0;}`);
     styleEl.sheet.insertRule(`.markdown .code-block__copy-button.${cssPrefix} button{ font-family: monospace; font-size: 13px;line-height: 13px; }`);
     // デフォルトのコードハイライトの高さを変える
-    styleEl.sheet.insertRule(`pre.highlight {line-height:12px;font-size:12px;font-family:SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace; }`);
+    styleEl.sheet.insertRule(`pre.highlight {line-height:12px;font-size:13px;font-family:SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace; }`);
   }
 }
 function copyText(str: string) {
   navigator.clipboard.writeText(str);
 }
-type ButtonType = { button: HTMLButtonElement, type: ("all" | "path") };
+type ButtonType = {button: HTMLButtonElement; type: 'all' | 'path'};
 function getButtons(filePath: string): ButtonType[] {
   const result: ButtonType[] = [];
   const splitPath = [...filePath.split(/[\\\/]/)];
   if (1 < splitPath.length) {
-    const element = document.createElement("button");
+    const element = document.createElement('button');
     element.innerText = filePath;
-    element.addEventListener("click", () => {
+    element.addEventListener('click', () => {
       copyText(filePath);
     });
     result.push({
       button: element,
-      type: "all"
+      type: 'all',
     });
   }
   for (let path of splitPath) {
-    const element = document.createElement("button");
+    const element = document.createElement('button');
     element.innerText = path;
-    element.addEventListener("click", () => {
+    element.addEventListener('click', () => {
       copyText(path);
     });
     result.push({
       button: element,
-      type: "path"
+      type: 'path',
     });
   }
   return result;
